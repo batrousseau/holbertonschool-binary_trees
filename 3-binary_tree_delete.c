3fd391ec2b5715b3007fb14adfda989bc1f9cb47
batrousseau <batrousseau@hotmail.com>
2025-09-15 19:41:45 +0200

    First push of the task

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete a binary tree
+ * @tree: pointer to root of the tree
+ * Return: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
