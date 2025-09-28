7861ecd9873923da4e38079ada113293b7d36665
batrousseau <batrousseau@hotmail.com>
2025-09-28 15:58:58 +0200

    First push of task 11

--- binary_trees.h
+++ binary_trees.h
@@ -35,6 +35,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 int is_superior(int a, int b);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 
 #endif
