5fbf5c006cfbccfd54d15a12ad916bd539b4ecb0
batrousseau <batrousseau@hotmail.com>
2025-09-28 15:50:03 +0200

    First push of task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - compute the depth of a
+ * node in a binary tree
+ * @tree: node to look at
+ * Return: 0 if null, otherwise depth
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+
+size_t depth = 0;
+
+if (tree->parent == NULL)
+{
+	return (0);
+}
+
+depth = 1 + binary_tree_depth(tree->parent);
+
+
+return (depth);
+
+}
--- binary_trees.h
+++ binary_trees.h
@@ -34,6 +34,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 int is_superior(int a, int b);
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 
 #endif
