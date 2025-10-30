0a7012502d504f92d63e922fb5b3d0f781a93c9c
batrousseau <batrousseau@hotmail.com>
2025-10-30 17:32:05 +0100

    testing new approach with leaves

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,6 +2,40 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_leaves- count leaves of a
+ * binary tree
+ * @tree: root node to count from
+ * Return: 0 if tree is NULL, otherwise leafs
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+size_t leafs = 0;
+size_t left = 0;
+size_t right = 0;
+
+
+if (tree == NULL)
+{
+	return (0);
+}
+
+if (tree->left == NULL && tree->right == NULL)
+{
+	return (1);
+}
+
+left = binary_tree_leaves(tree->left);
+right = binary_tree_leaves(tree->right);
+
+leafs = left + right;
+
+return (leafs);
+}
+
+
 /**
  * binary_tree_height - measures the height of
  * a binary tree
@@ -125,6 +159,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int balanced = 0;
 	int full = 0;
 	int perfect = 0;
+	int leaf_number = 0;
 
 	if (tree == NULL)
 	{
@@ -133,8 +168,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	balanced = binary_tree_balance(tree);
 	full = binary_tree_is_full(tree);
+	leaf_number = binary_tree_leaves(tree);
 
-	if (balanced == 0 && full == 1)
+	if (balanced == 0 && full == 1 && leaf_number % 2 == 0)
 	{
 		perfect = 1;
 	}
